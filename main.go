368ab76f8a9c0734ae74d642d069dca2ba50b69a
Sunil <SunilKrishna.Sodankur@anz.com>
2023-04-22 13:12:05 +1000

    Port updated

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	router.HandleFunc("/api/upload", apis.UploadFile).Methods("POST")
 	router.HandleFunc("/api/download", apis.DownloadFiles).Methods("GET")
 
-	err := http.ListenAndServe(":5000", router)
+	err := http.ListenAndServe(":8282", router)
 
 	if err != nil {
 		fmt.Println(err)
